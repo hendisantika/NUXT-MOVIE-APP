15db43049f59414ce1c80723293117b07be42fa0
hendisantika <hendisantika@yahoo.co.id>
2024-01-30 15:18:06 +0700

    Modify nuxt.config.ts ✏️📗 :octocat:⬆

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: true}
+    modules: [
+        'nuxt-windicss',
+        '@vueuse/nuxt'
+    ],
+    runtimeConfig: {
+        apiKey: '',
+        apiBaseUrl: '',
+        // We use the public runtime config in
+        //order to expose this also to the client side
+        public: {
+            imgBaseUrl: '',
+        }
+    }
 })
